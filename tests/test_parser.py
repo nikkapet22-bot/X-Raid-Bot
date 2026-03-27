cd67327094a89dc59fd0fd843171d013ecbf3890
zztop <nikkapet22@gmail.com>
2026-03-27 16:59:28 +0200

    feat: normalize raid markers across supported bots

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -9,6 +9,23 @@ Replies 7 | 7 [%]
 https://x.com/i/status/2036140112963313949
 """
 
+D_RAIDBOT_MESSAGE = """
+Reposts 8 | 8 [%]
+Bookmarks 4 | 4 [%]
+Replies 2 | 2 [%]
+
+https://x.com/i/status/999
+"""
+
+SINGULAR_MARKER_MESSAGE = """
+Like 1
+Retweet 1
+Reply 1
+Bookmark 1
+
+https://twitter.com/some_user/status/1234567890123456789
+"""
+
 QUEUE_MESSAGE = """
 Next up... (7/10 tweets)
 
@@ -21,7 +38,38 @@ def test_parse_raid_message_extracts_canonical_status_url():
     match = parse_raid_message(ACTIVE_MESSAGE)
 
     assert match is not None
+    assert match.raw_url == "https://x.com/i/status/2036140112963313949"
     assert match.normalized_url == "https://x.com/i/status/2036140112963313949"
+    assert match.requirements.like is True
+    assert match.requirements.repost is True
+    assert match.requirements.reply is True
+    assert match.requirements.bookmark is False
+
+
+def test_parse_raid_message_extracts_required_actions_from_d_raidbot_style_message():
+    match = parse_raid_message(D_RAIDBOT_MESSAGE)
+
+    assert match is not None
+    assert match.normalized_url == "https://x.com/i/status/999"
+    assert match.requirements.repost is True
+    assert match.requirements.bookmark is True
+    assert match.requirements.reply is True
+    assert match.requirements.like is False
+
+
+def test_parse_raid_message_normalizes_singular_synonyms_and_twitter_source_url():
+    match = parse_raid_message(SINGULAR_MARKER_MESSAGE)
+
+    assert match is not None
+    assert (
+        match.raw_url
+        == "https://twitter.com/some_user/status/1234567890123456789"
+    )
+    assert match.normalized_url == "https://x.com/some_user/status/1234567890123456789"
+    assert match.requirements.like is True
+    assert match.requirements.repost is True
+    assert match.requirements.reply is True
+    assert match.requirements.bookmark is True
 
 
 def test_parse_raid_message_rejects_next_up_queue_posts():
